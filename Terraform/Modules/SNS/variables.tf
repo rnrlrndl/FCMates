41cae73c320d75f16c79f468605e9c38051c6b06
rnrlrndl <142502484+rnrlrndl@users.noreply.github.com>
2025-11-04 16:45:25 +0900

    Add notification_email variable to variables.tf
    Added notification_email variable for SNS notifications.

--- Terraform/Modules/SNS/variables.tf
+++ Terraform/Modules/SNS/variables.tf
@@ -1,4 +1,12 @@
 variable "kms_key_id" {
   description = "KMS key ID for SNS topic encryption"
   type        = string
+
+}
+
+# 이메일 알림 설정 변수
+variable "notification_email" {
+  description = "Email address for SNS notifications"
+  type        = string
+  # 기본값 없음 - main.tf에서 반드시 입력해야 함
 }
